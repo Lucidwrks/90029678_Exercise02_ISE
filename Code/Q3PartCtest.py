03e6821d96b638ae94ee8fcd19327d327003ae27
Jovin <jovinog@outlook.com>
2023-05-18 15:30:26 +0800

    mmmmm

---
+++ Code/Q3PartCtest.py
@@ -0,0 +1,8 @@
+from Q3PartC import pie_divider
+import unittest
+
+class Q3PartCtest(unittest.TestCase):
+    def pie_divider_test(self):
+        num_people = 2
+        num_pies = 5
+        capout = io.StringIO 
